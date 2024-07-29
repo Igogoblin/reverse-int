318450b7c40c2f17cf7cbb782b61e8ee78d228b2
Ihar Skavysh <igogoblin@gmail.com>
2024-07-29 21:19:17 +0300

    refactor: change if on ?: and Number to +

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 module.exports = function reverse(n) {
-    if (n < 0) {
-        let a = n * -1;
-        return Number(a.toString().split("").reverse().join(""));
-    }
-    return Number(n.toString().split("").reverse().join(""));
+    // if (n < 0) {
+    //     let a = n * -1;
+    //     return Number(a.toString().split("").reverse().join(""));
+    // }
+    return n < 0
+        ? +(n * -1).toString().split("").reverse().join("")
+        : +n.toString().split("").reverse().join("");
 };
